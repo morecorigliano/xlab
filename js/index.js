d186ae3bea039e50a9dae54a8f9d3c1029d336d0
morena <morena@dsklabs.com>
2020-10-01 02:53:38 -0300

    avance 30/9

--- js/index.js
+++ js/index.js
@@ -49,3 +49,38 @@ for (var i = 0; i < formInputs.length; i++) {
         }
     });
 }
+
+////// offices ////////
+
+var adress = document.getElementsByClassName("adress");
+var overline = document.getElementById("overline");
+var initial = document.getElementById("initial");
+
+for (var i = 0; i < adress.length; i++) {
+    adress[i].addEventListener("click", function(){
+      initial.classList.remove("initial");
+      for (var i = 0; i < adress.length; i++) {
+        adress[i].children[1].classList.remove("show");
+        adress[i].children[1].style.bottom = "-.5rem";
+        adress[i].children[1].style.opacity = "0";
+      }
+      this.children[1].classList.add("show");
+      var current = this;
+      window.setTimeout( function(){
+        current.children[1].style.bottom = "0";
+        current.children[1].style.opacity = "1";
+      }, 60);
+      if(this.classList.contains("one")){
+        overline.style.top = "0";
+      }
+      if(this.classList.contains("two")){
+        overline.style.top = "1.4rem";
+      }
+      if(this.classList.contains("three")){
+        overline.style.top = "2.8rem";
+      }
+      if(this.classList.contains("four")){
+        overline.style.top = "4.2rem";
+      }
+    });
+}
